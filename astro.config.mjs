fa70557cde97d9a1236c136642259b9692e5402f
kabhishek1782 <kabhishekf17@gmail.com>
2025-10-25 18:05:48 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { remarkReadingTime } from './src/lib/remark.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:/kabhishek1782.github.io',
+  site: 'https:/kabhishek1782.github.io/personal-website',
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
